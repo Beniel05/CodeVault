52aa0017eeef564fd3c99f4ef62d415e6f6e9a77
beniel05 <benielebenezer@gmail.com>
2025-12-12 23:48:13 +0530

    DSA: Updated basic declare - instantiate - initialize steps (A00_Creating2DArray)

--- DSA/src/arrays/twoDimensionArray/A00_Creating2DArray.java
+++ DSA/src/arrays/twoDimensionArray/A00_Creating2DArray.java
@@ -4,6 +4,23 @@ import java.util.Arrays;
 
 public class A00_Creating2DArray {
     public static void main(String[] args) {
+
+        // Step 1 - Declare
+        int[][] arrayName;
+
+        // Step 2 - Instantiate
+        arrayName = new int[2][2];
+
+        // Step 3 - Initialize
+        arrayName[0][0] = 1;
+        arrayName[0][1] = 2;
+        arrayName[1][0] = 3;
+        arrayName[1][1] = 4;
+
+        System.out.println(Arrays.deepToString(arrayName));
+
+        System.out.println("\n========================================\n");
+
         int[][] int2DArray = {{1, 2}, {3, 4}};
         String[][] str2DArray = {{"Welcome to Java!"}, {"This is 2D Array"}};
         char[][] char2DArray = {{'a', 'b', 'c'}, {'y', 'z'}};
@@ -19,9 +36,9 @@ public class A00_Creating2DArray {
         // Row-by-row (matrix-like) output
         System.out.println("Row-by-row output (matrix style):\n");
         printRows(int2DArray);
-        System.out.println();
+        System.out.println(); // for empty line after the previous method's execution
         printRows(str2DArray);
-        System.out.println();
+        System.out.println(); // for empty line after the above method's execution
         printRows(char2DArray);
     }
 
