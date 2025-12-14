5a82fcf83a8854cc811afcdd6dbb753c3aa117f2
beniel05 <benielebenezer@gmail.com>
2025-12-14 14:45:33 +0530

    DSA: Added index-based logical deletion in 2D array with O(1) complexity (A06_DeletingElementIn2DArray)

---
+++ DSA/src/arrays/twoDimensionArray/A06_DeletingElementIn2DArray.java
@@ -0,0 +1,37 @@
+package arrays.twoDimensionArray;
+
+public class A06_DeletingElementIn2DArray {
+
+    static int[][] arr = {
+            {1, 33, 55, 91},
+            {5, 4, 10, 11}, // first 10
+            {24, 50, 10, 40} // second 10
+    };
+
+    static final int EMPTY = Integer.MIN_VALUE;
+
+    public static void main(String[] args){
+        deleteIndex(0,3);
+        deleteIndex(2,4);
+    }
+
+    // Time Complexity: O(1)
+    // Space Complexity: O(1)
+    static void deleteIndex(int rowIndex, int colIndex) {
+
+        // IndexOutOfBound checking;
+        if (rowIndex < 0 || rowIndex >= arr.length || colIndex < 0 || colIndex >= arr[rowIndex].length) {
+            System.out.println("(" + rowIndex + "," + colIndex + ") Invalid index access");
+            return;
+        }
+
+        System.out.println("Before deleting the element: " + arr[rowIndex][colIndex]);
+
+        // Updating the given index's value as EMPTY ( -2^31 ) Logical deletion
+        arr[rowIndex][colIndex] = EMPTY;
+        System.out.println("Deleted element at Row: " + rowIndex + " Col: " + colIndex);
+
+        System.out.println("After deleting the element: " + arr[rowIndex][colIndex]);
+    }
+
+}
