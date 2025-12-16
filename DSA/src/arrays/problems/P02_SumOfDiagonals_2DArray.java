ff7348a16eaab4db8f5ea3f80a4b728e59c57483
beniel05 <benielebenezer@gmail.com>
2025-12-16 15:59:49 +0530

    Added problem statement and minor refinements (P02_SumOfDiagonals_2DArray)

--- DSA/src/arrays/problems/P02_SumOfDiagonals_2DArray.java
+++ DSA/src/arrays/problems/P02_SumOfDiagonals_2DArray.java
@@ -1,4 +1,36 @@
 package arrays.problems;
+/*
+Problem Statement
+
+Write two functions to calculate the sum of diagonal elements in a 2D integer array.
+- `sumDiagonalElements` - returns the sum of 'primary diagonal' elements
+- `sumAntiDiagonalElements` - returns the sum of 'anti-diagonal' elements
+
+Definitions
+- Primary Diagonal: `array[i][i]`
+- Anti-Diagonal: `array[n - 1 - i][i]`
+- Diagonals are defined only for square matrices (N × N)
+
+Examples
+- Example 1 – Primary Diagonal
+Input: {
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+};
+Output: 15
+Explanation: 1 + 5 + 9 = 15
+
+- Example 2 – Anti-Diagonal
+Input: {
+    {1, 2, 3},
+    {4, 5, 6},
+    {0, 8, 9}
+};
+Output: 8
+Explanation: 0 + 5 + 3 = 8
+*/
+
 
 public class P02_SumOfDiagonals_2DArray {
 
@@ -13,21 +45,18 @@ public class P02_SumOfDiagonals_2DArray {
 
         int[][] arr2 = {{1,2,3},{4,5,6},{0,8,9}}; // 0 in place of 7 for clarity
 
-        // Sum of primary diagonal elements
         System.out.println(sumDiagonalElements(arr1));
-
-        // Sum of anti-diagonal elements
         System.out.println(sumAntiDiagonalElements(arr2));
     }
 
     // Time Complexity: O(n) | Space Complexity: O(1)
-    // Sums elements where row index == column index (primary diagonal)
+    // Sums elements where row index == column index (primary diagonal) ( 1 - 5 - 9 )
     static int sumDiagonalElements(int[][] array) {
 
         int diagonalSum = 0;
-        int length = array.length; // Number of rows (also columns for square matrix)
+        int n = array.length; // Number of rows (also columns for square matrix)
 
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < n; i++) {
             // Primary diagonal condition: array[i][i]
             diagonalSum += array[i][i];
         }
@@ -36,18 +65,16 @@ public class P02_SumOfDiagonals_2DArray {
     }
 
     // Time Complexity: O(n) | Space Complexity: O(1)
-    // Sums elements where column index = (n - 1 - row index) (anti-diagonal)
+    // Sums elements where column index = (n - 1 - row index) (anti-diagonal) ( 0 - 5 - 3 )
     static int sumAntiDiagonalElements(int[][] array) {
 
         int antiDiagonalSum = 0;
-        int length = array.length; // Matrix dimension
+        int n = array.length; // Matrix dimension
 
-        for (int i = length - 1; i >= 0; i--) {
-            // Maps (row, col) as:
-            // row = length - 1 - i
-            // col = i
-            antiDiagonalSum += array[length - 1 - i][i];
+        for (int i = 0; i < n; i++) {
+            antiDiagonalSum += array[n - 1 - i][i];
         }
+
         return antiDiagonalSum;
     }
 }
