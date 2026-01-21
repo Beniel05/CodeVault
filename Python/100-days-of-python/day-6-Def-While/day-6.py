d70c9909903a67030588c3e19eb1674f62d0f0e5
beniel05 <benielebenezer@gmail.com>
2026-01-21 21:29:14 +0530

    Day-6 of Python - learned about functions and while loop

---
+++ Python/100-days-of-python/day-6-Def-While/day-6.py
@@ -0,0 +1,15 @@
+# Learned about while loop, indentation and function using: https://reeborg.ca/reeborg.html
+# Practiced there with inbuilt and self created functions - (Hurdle 1 to 4) challenges
+
+print(len("Hello"))
+def my_function(a, b):
+    print("The sum of a and b is:", a + b)
+my_function(5,10)
+
+def inden():
+ print("Hello World") # Just one space (indent) still works. /* Not a good practice!
+
+count = 5
+while count > 0:
+    inden()
+    count -= 1
