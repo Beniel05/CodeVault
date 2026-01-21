6daa2f9fb11f02bf236cde46ea82a974f4cabd95
beniel05 <benielebenezer@gmail.com>
2026-01-21 06:29:26 +0530

    Day-6 JS started to practice DOM

---
+++ Javascript/DailyPractice/day-6-GuessTheNumber/script.js
@@ -0,0 +1,9 @@
+const dq = document.querySelector.bind(document);
+console.log(dq(".message").textContent);
+document.querySelector('.message').textContent = "Correct Number"
+
+document.querySelector('.number').textContent = 15;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 11;
+console.log(document.querySelector('.guess').value);
