fd78743a8946af2f3ec50ea64e77e7e2410127ee
beniel05 <benielebenezer@gmail.com>
2026-01-23 07:31:51 +0530

    Day-7 -JS- Finished building the (Guess the number) game

--- Javascript/DailyPractice/day-6-GuessTheNumber/script.js
+++ Javascript/DailyPractice/day-6-GuessTheNumber/script.js
@@ -1,3 +1,74 @@
+/* GAME LOGIC */
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
+// const randomNumber = Math.floor(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+console.log(randomNumber); // DEVELLOPMENT PURPOSE
+
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  user_number = Number(document.querySelector(".guess").value);
+  // output_message = document.querySelector(".message"); // Created a function for this above
+
+  // If user clicks the check button without entering any value/number
+  if (!user_number) {
+    displayMessage("Enter a number!");
+  }
+  // If the user guessed the correct number
+  else if (user_number === randomNumber) {
+    if (highScore < score) {
+      document.querySelector(".highscore").textContent = score;
+    }
+    document.querySelector(".number").textContent = randomNumber;
+    displayMessage("You Got that right!");
+    document.body.style.backgroundColor = "green";
+    document.querySelector(".number").style.width = "20rem";
+  } else if (user_number !== randomNumber) {
+    displayMessage(user_number > randomNumber ? "Too High" : "Too Low");
+    score--;
+    document.querySelector(".score").textContent = score;
+  }
+  // // If the user guess is too high
+  // else if (user_number >= randomNumber) {
+  //   if (score > 0) {
+  //     output_message.textContent = "Too High";
+  //     score--;
+  //     document.querySelector(".score").textContent = score;
+  //   }
+  // }
+  // // If the user guess is too low
+  // else if (user_number <= randomNumber) {
+  //   if (score > 0) {
+  //     output_message.textContent = "Too Low";
+  //     score--;
+  //     document.querySelector(".score").textContent = score;
+  //   }
+  // }
+  // If the user guess wrong answers 20 times in a row
+  if (score === 0) {
+    document.querySelector(".number").textContent = randomNumber;
+    displayMessage("You have lost.");
+    document.body.style.backgroundColor = "red";
+  }
+});
+
+// When the user click the again button
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  randomNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage("Start guessing...");
+  document.querySelector(".score").textContent = score; // score = 20
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+});
+/* END */
+
 // const dq = document.querySelector.bind(document);
 // console.log(dq(".message").textContent);
 // document.querySelector('.message').textContent = "Correct Number"
@@ -31,43 +102,3 @@
 //         document.querySelector(".message").textContent = "No number entered"
 //     }
 // })
-
-/* GAME LOGIC */
-const randomNumber = Math.trunc(Math.random() * 20) + 1;
-// const randomNumber = Math.floor(Math.random() * 20) + 1;
-let score = 20;
-let highScore = 0;
-// document.querySelector(".number").textContent = randomNumber;
-console.log(randomNumber);
-
-document.querySelector(".check").addEventListener("click", function () {
-  user_number = Number(document.querySelector(".guess").value);
-  output_message = document.querySelector(".message");
-
-  if (!user_number) {
-    output_message.textContent = "Enter a number!";
-  } else if (user_number === randomNumber) {
-    if (highScore < score) {
-      highScore = score;
-    }
-    output_message.textContent = "You Got that right!";
-    document.body.style.backgroundColor = "green";
-  } else if (user_number >= randomNumber) {
-    if (score > 0) {
-      output_message.textContent = "Too High";
-      score--;
-      document.querySelector(".score").textContent = score;
-    }
-  } else if (user_number <= randomNumber) {
-    if (score > 0) {
-      output_message.textContent = "Too Low";
-      score--;
-      document.querySelector(".score").textContent = score;
-    }
-  }
-  if (score === 0) {
-    document.querySelector(".number").textContent = randomNumber;
-    output_message.textContent = "You have lost.";
-    document.body.style.backgroundColor = "red";
-  }
-});
